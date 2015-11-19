87b309a78535b432188c5c8c231b11dcc26a0dcb
Miroslav Ligas <miroslav.ligas@liferay.com>
2015-11-19 08:45:38 +0000

    step config

--- src/main/webapp/js/tutorial_setup.js
+++ src/main/webapp/js/tutorial_setup.js
@@ -46,8 +46,11 @@ AUI.add(
                 NAME: 'tutorial-setup',
 
                 ATTRS: {
-                    position:{
+                    position: {
                         value : DEFAULT_POSITON
+                    },
+                    elem: {
+                        value:null
                     }
                 },
 
@@ -58,9 +61,6 @@ AUI.add(
 
                         var mainContent = A.Node.create(POPOVER_CONTENT);
                         var footerContent = A.Node.create(POPOVER_FOOTER);
-
-                        instance._container = new Liferay.TutorialSetupContainer();
-
 
                         var position = POSITIONS[DEFAULT_POSITON];
 
@@ -76,24 +76,31 @@ AUI.add(
                                 zIndex: 20
                             }
                         );
-                        instance._popover.render();
 
-                        A.one('.tutorial-setup-popover .btn-primary').on('click', function () {
+                        footerContent.one('.btn-primary').on('click', function () {
                             var value = instance._popover.get('boundingBox').one(".content").get('value');
-                            instance._container.addTutorialStep(value);
                             instance._popover.set('visible', false);
+
+                            instance.fire('tutorialStepSave',
+                                {
+                                    'text':value,
+                                    'position':instance.get('position'),
+                                    'node':instance.get('elem')
+                                });
                         });
-                        A.one('.tutorial-setup-popover .btn-left').on('click', function () {
+                        footerContent.one('.btn-left').on('click', function () {
                             var index = instance.get('position');
 
                             instance._setPosition(--index)
                         });
-                        A.one('.tutorial-setup-popover .btn-right').on('click', function () {
+                        footerContent.one('.btn-right').on('click', function () {
                             var index = instance.get('position');
 
                             instance._setPosition(++index)
                         });
 
+                        instance._popover.set('visible', false);
+                        instance._popover.render();
 
                         var page = A.one('#wrapper');
 
@@ -108,11 +115,10 @@ AUI.add(
 
                                 elem.setStyle('border', '2px solid yellow');
 
-
-                                instance._popover.set('visible', true);
+                                instance._elem = elem;
                                 instance. _setPosition(DEFAULT_POSITON);
                                 instance._popover.get('boundingBox').one(".content").set('value','');
-                                instance._elem = elem;
+                                instance._popover.set('visible', true);
                                 return false;
                             });
                     },
@@ -134,6 +140,96 @@ AUI.add(
 
         Liferay.TutorialSetup = TutorialSetup;
 
+        var SETUP_CONFIG_TEMPLATE =
+            '<div class="tutorial-step-config">' +
+            '   <select>' +
+            '      <option value="click">Click</option>' +
+            '      <option value="dbclick">Double</option>' +
+            '      <option value="http">Go to address</option>' +
+            '   </select>' +
+            '   <input type="text"/>' +
+            '</div>';
+
+
+        var TutorialSetupConfig = A.Component.create(
+            {
+                EXTENDS: A.Base,
+
+                NAME: 'tutorial-setup-config',
+
+                prototype: {
+
+                    initializer: function (config) {
+                        var instance = this;
+
+                        var bodyContent = A.Node.create(SETUP_CONFIG_TEMPLATE);
+
+                        instance._modal = new A.Modal(
+                            {
+                                bodyContent: bodyContent,
+                                centered: true,
+                                headerContent: '<h3>Configuration</h3>',
+                                modal: true,
+                                zIndex: 1000,
+                                width: 450
+                            }
+                        );
+
+                        instance._modal.addToolbar(
+                            [
+                                {
+                                    cssClass: 'btn-save',
+                                    label: 'Save',
+                                    on: {
+                                        click: A.bind('_onSaveClick', instance)
+                                    }
+                                }
+                            ]
+                        );
+
+                        instance._modal.render();
+                        instance._modal.hide();
+
+                        instance._input = bodyContent.one('input');
+                        instance._select = bodyContent.one('select');
+
+                        instance._input.hide();
+
+                        instance._select.on('change',function(){
+                            if(this.get('value') == 'http') {
+                                instance._input.set('value','');
+                                instance._input.show();
+                            }else{
+                                instance._input.hide();
+                            }
+                        });
+
+                    },
+                    show:function(state) {
+                        var instance = this;
+
+                        instance._select.set('value',state.option);
+                        instance._input.set('value',state.value);
+                        instance._modal.show();
+                    },
+                    hide:function() {
+                        var instance =this;
+                        instance._modal.hide();
+                    },
+                    _onSaveClick: function(){
+                        var instance =this;
+
+                        var select = instance._select.get('value');
+                        var input = instance._input.get('value');
+
+                        instance.fire('setupConfigSave',{'option':select,'value':input});
+                        instance._modal.hide();
+                    }
+                }
+            });
+
+        Liferay.TutorialSetupConfig = TutorialSetupConfig;
+
         var TutorialSetupContainer = A.Component.create(
             {
                 EXTENDS: A.Base,
@@ -147,20 +243,12 @@ AUI.add(
 
                         instance._container = A.Node.create(CONFIG_TEMPLATE_WRAPPER);
 
-                        instance._modal = new A.Modal(
-                            {
-                                bodyContent: 'Modal body',
-                                centered: true,
-                                headerContent: '<h3>Modal header</h3>',
-                                modal: true,
-                                //render: '#modal',
-                                width: 450
-                            }
-                        ).render();
-                        instance._modal.hide();
+                        instance._setup = new Liferay.TutorialSetup();
+
+                        instance._setupConfig = new Liferay.TutorialSetupConfig();
 
                         instance._container.delegate('click',function(){
-                            instance._modal.show();
+                            instance._setupConfig.show({});
                         },'.btn-setup');
 
                         instance._sortableLayout = new A.SortableList(
@@ -174,6 +262,14 @@ AUI.add(
                         );
 
                         A.one('body').appendChild(instance._container);
+
+                        instance._setup.on('tutorialStepSave', function(event) {
+                            instance.addTutorialStep(event['text']);
+                        });
+
+                        instance._setupConfig.on('setupConfigSave', function(event) {
+
+                        });
                     },
                     addTutorialStep : function(text){
                         var instance = this;
